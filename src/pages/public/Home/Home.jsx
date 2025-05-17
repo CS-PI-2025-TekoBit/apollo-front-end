1f4cf796502dc4a490e9726a0887930200c80248
Rafael Scarpelli <rafaelscarpelli715@gmail.com>
2025-05-17 14:44:44 -0300

    add | Selects do filtro

--- src/pages/public/Home/Home.jsx
+++ src/pages/public/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../../components/Header/Header'
 import Footer from '../../../components/Footer/Footer'
 import Maps from '../../../components/Maps/Maps'
@@ -6,23 +6,79 @@ import './Home.css'
 import { useAllCars } from '../../../hooks/useAllCar'
 import { useFilters } from '../../../hooks/useFilters'
 import { useAuth } from '../../../hooks/useAuth';
+import GenericSelect from '../../../components/GenericSelect/GenericSelect'
 
 export default function Home() {
     const { cars } = useAllCars()
     const { filtros } = useFilters()
     const { user } = useAuth()
 
+    const [selectedMark, setSelectedMark] = useState('');
+    const [selectedModel, setSelectedModel] = useState('');
+    const [selectedMotor, setSelectedMotors] = useState('');
+    const [selectedColor, setSelectedColor] = useState('');
+    
+    const filteredModels = filtros?.models.filter(model => model.mark === selectedMark);
+
+    const handleMarkChange = (e) => {
+        setSelectedMark(e.target.value);
+        setSelectedModel(''); 
+    };
+
+    const handleModelChange = (e) => {
+        setSelectedModel(e.target.value);
+    };
+
+    const handleMotorChange = (e) => {
+        setSelectedMotors(e.target.value);
+    };
+
+    const handleColorChange = (e) => {
+        setSelectedColor(e.target.value);
+    };
+
     return (
         <>
             <Header />
-            {console.log("carros", cars)}
             <main>
                 <div className="container-stock ">
                     <div className={`left-side-stock`} >
-                        <h1 className={`title-filters`}>
+                        <h1 className="title-filters">
                             Filtros
                             <div className="filters">
-                                <h1>FILTROS AQUI</h1>
+                                {/* Aqui você insere o select */}
+                                <GenericSelect
+                                    label="Selecione a Marca"
+                                    placeholder="Selecione uma marca"
+                                    options={filtros?.marks}
+                                    value={selectedMark}
+                                    onChange={handleMarkChange}
+                                />
+
+                                <GenericSelect
+                                    label="Selecione o Modelo"
+                                    placeholder="Selecione um modelo"
+                                    options={filteredModels}
+                                    value={selectedModel}
+                                    onChange={handleModelChange}
+                                    disabled={!selectedMark} // Desabilita se não houver marca selecionada
+                                />
+
+                                <GenericSelect
+                                    label="Selecione o Motor"
+                                    placeholder="Selecione um motor"
+                                    options={filtros?.motors}
+                                    value={selectedMotor}
+                                    onChange={handleMotorChange}
+                                />
+
+                                <GenericSelect
+                                    label="Selecione a Cor"
+                                    placeholder="Selecione uma cor"
+                                    options={filtros?.colors}
+                                    value={selectedColor}
+                                    onChange={handleColorChange}
+                                />
                             </div>
                         </h1>
                     </div>
