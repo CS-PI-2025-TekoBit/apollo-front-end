44bf8e5cd04cf798d0fd9fa7decd14959639a9ce
Daniele Fonseca <danieledasilvafonseca@gmail.com>
2025-05-21 13:51:32 -0300

    add | envio para whatsapp com mensagem

--- src/components/BotaoWhatsApp/BotaoWhatsApp.jsx
+++ src/components/BotaoWhatsApp/BotaoWhatsApp.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import './BotaoWhatsApp.css';
-import { WhatsappLogo } from '@phosphor-icons/react'
+import { WhatsappLogo } from '@phosphor-icons/react';
 
+export default function BotaoWhatsApp() {
+    const abrirWhatsApp = (numero) => {
+        const mensagem = encodeURIComponent("Olá, gostaria de mais informações sobre um veículo.");
+        window.open(`https://wa.me/${numero}?text=${mensagem}`, '_blank');
+    };
 
-export default function Footer() {
     return (
-       <div className="whatsapp-fixo">
-            <div className="contato">
+        <div className="whatsapp-fixo">
+            <div className="contato" onClick={() => abrirWhatsApp('5544991535404')}>
                 <p>Wagner</p>
                 <p>(44) 9 99153-5404</p>
             </div>
-            <div className="contato">
+            <div className="contato" onClick={() => abrirWhatsApp('5544991535404')}>
                 <p>José Luiz</p>
                 <p>(44) 9 99153-5404</p>
             </div>
