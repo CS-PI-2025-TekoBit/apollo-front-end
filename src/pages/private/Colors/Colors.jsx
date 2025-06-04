8da8e95a163dc142cc905eed3868ed4f32220dab
Daniele Fonseca <danieledasilvafonseca@gmail.com>
2025-06-04 13:58:09 -0300

    feat | alteração na listagem de cores

--- src/pages/private/Colors/Colors.jsx
+++ src/pages/private/Colors/Colors.jsx
@@ -20,79 +20,111 @@ function Colors() {
     const { colors, isLoading } = useColors();
     const navigate = useNavigate();
     const queryClient = useQueryClient();
+
     const dtCadBodyTemplate = (rowData) => {
         return (
             rowData.dt_create ? new Date(rowData.dt_create).toLocaleDateString('pt-BR') : 'N/A'
         );
     }
 
-
-    const actionBodyTemplate = (rowData) => {
-            return (
-                <div className="btn-action">
-                    <Button
-                        icon={<Edit size={20} weight='fill' color='white' />}
-                        className="btn-edit"
-                        label='Editar'
-                        onClick={() =>
-                            navigate('/admin/colors/register', {
-                                state: {
-                                    id: rowData.id_colors,
-                                    pageName: `021 - Edição de cor`,
-                                    pageTitle: 'Editar Cor',
-                                    labelNameForm: 'Nome da Cor',
-                                    routeEdit: '/colors/edit',
-                                    initialData: {
-                                        name: rowData.name,
-                                        status: rowData.status,
-                                    }
-                                },
-                            })
-                        }
-                    />
-                    <Button
-                        icon={<XCircle size={20} weight='fill' color='white' />}
-                        className="btn-delete"
-                        label='Excluir'
-                        onClick={() => {
-                            Swal.fire({
-                                title: 'Excluir cor',
-                                text: `Tem certeza que deseja excluir a cor ${rowData.name}?`,
-                                icon: 'warning',
-                                showCancelButton: true,
-                                confirmButtonColor: '#3085d6',
-                                cancelButtonColor: '#d33',
-                                confirmButtonText: 'Sim, excluir!',
-                                reverseButtons: true,
-                                focusCancel: true,
-                                focusConfirm: false,
-                                customClass: {
-                                    popup: 'sweet-alert-zindex'
-                                },
-                                cancelButtonText: 'Cancelar'
-                            }).then(async (result) => {
-                                if (result.isConfirmed) {
-                                    toast.success(`Cor ${rowData.name} excluída com sucesso!`);
-                                    return
-                                }
-                            })
-                        }
-                        }
-                    />
-                </div>
-            );
-        }
-
-    
     const statusBodyTemplate = (rowData) => {
         return rowData.status === 'active' ? "Ativo" : "Inativo";
     }
+
     const rowClassName = (data, index) => {
         return index % 2 === 0 ? 'even-row' : 'odd-row';
     };
 
+    const actionBodyTemplate = (rowData) => {
+        return (
+            <div className="btn-action">
+                <Button
+                    icon={<Edit size={20} weight='fill' color='white' />}
+                    className="btn-edit"
+                    label='Editar'
+                    onClick={() =>
+                        navigate('/admin/colors/register', {
+                            state: {
+                                id: rowData.id_colors,
+                                pageName: `021 - Edição de cor`,
+                                pageTitle: 'Editar Cor',
+                                labelNameForm: 'Nome da Cor',
+                                routeEdit: '/colors/edit',
+                                initialData: {
+                                    name: rowData.name,
+                                    status: rowData.status,
+                                }
+                            },
+                        })
+                    }
+                />
+                <Button
+                    icon={<XCircle size={20} weight='fill' color='white' />}
+                    className="btn-delete"
+                    label='Excluir'
+                    onClick={() => {
+                        Swal.fire({
+                            title: 'Excluir cor',
+                            text: `Tem certeza que deseja excluir a cor ${rowData.name}?`,
+                            icon: 'warning',
+                            showCancelButton: true,
+                            confirmButtonColor: '#3085d6',
+                            cancelButtonColor: '#d33',
+                            confirmButtonText: 'Sim, excluir!',
+                            reverseButtons: true,
+                            focusCancel: true,
+                            focusConfirm: false,
+                            customClass: {
+                                popup: 'sweet-alert-zindex'
+                            },
+                            cancelButtonText: 'Cancelar'
+                        }).then(async (result) => {
+                            if (result.isConfirmed) {
+                                toast.success(`Cor ${rowData.name} excluída com sucesso!`);
+                                return
+                            }
+                        })
+                    }
+                    }
+                />
+            </div>
+        );
+    }
+
+
     return (
-        <></>
+        isLoading ? (
+            <GenericLoader />
+        ) : (
+            <main style={{ position: 'relative', padding: '20px', zIndex: 20000 }} className='w-full'>
+                <section className="header-list w-full">
+                    <h3 className="text-header">002 - Listagem de Cores</h3>
+                    <br />
+                </section>
+                <section className="title-page">
+                    <div style={{ padding: '20px' }}> <h1 className='title'> Listagem de Cores</h1></div>
+                </section>
+                <section className="content-list">
+                    <div className="search-and-include">
+                        <div className="search">
+                            <input type="text" placeholder="Pesquisar" />
+                            <Button icon={<Search size={20} color='white' />} iconPos='left' className="button-search" />
+                        </div>
+                        <div className="include">
+                            <NavLink to="/admin/colors/register">
+                                <Button
+                                    label="Cadastrar Cor"
+                                    icon={<Palette size={30} weight='fill' />}
+                                    className="button-include"
+                                    onClick={() => console.log('Cadastrar Cor')}
+                                />
+                            </NavLink>
+                        </div>
+                    </div>
+                    {/* data table aqui */}
+                </section>
+            </main >
+        )
     );
 }
 
