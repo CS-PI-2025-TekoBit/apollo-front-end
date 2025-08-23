e441140ac595c4ee81156cbf32d8b83b421c4c27
rafaelscarpelli <rafaelscarpelli715@gmail.com>
2025-08-22 21:50:31 -0300

    incompleto | tela para edição de perfil

---
+++ src/pages/public/Editar/Editar.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Header from "../../../components/Header/Header";
+
+
+export default function Editar(){
+
+    return (
+        <>
+            < Header />
+
+        </>
+    )
+}
