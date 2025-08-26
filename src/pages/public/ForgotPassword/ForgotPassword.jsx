878cc0c567c13cad51e445c27a3aece0742095f2
danisfon <danieledasilvafonseca@gmail.com>
2025-08-26 20:57:09 -0300

    fix | continuando recperacao de senha

--- src/pages/public/ForgotPassword/ForgotPassword.jsx
+++ src/pages/public/ForgotPassword/ForgotPassword.jsx
@@ -1,74 +1,82 @@
-// import React, { useState } from 'react';
-// import { useNavigate } from 'react-router';
-// import { toast } from 'react-toastify';
-// import './ForgotPassword.css'; // Importa o CSS da tela de recuperação de senha
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+import './ForgotPassword.css';
+import { At } from '@phosphor-icons/react';
+import SLA from '../../../assets/imgs/logomarca.png'
 
-// function ForgotPassword() {
-//   const [email, setEmail] = useState('');
-//   const [erro, setErro] = useState('');
-//   const [enviando, setEnviando] = useState(false);
-//   const navigate = useNavigate();
+function ForgotPassword() {
+    const [email, setEmail] = useState('');
+    const [erro, setErro] = useState('');
+    const [enviando, setEnviando] = useState(false);
+    const navigate = useNavigate();
 
-//   const validacao = () => {
-//     if (email === '') {
-//       setErro('Campo e-mail é obrigatório');
-//       return false;
-//     }
-//     setErro('');
-//     return true;
-//   }
+    const validarEmail = (valor) => {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(valor);
+    };
 
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     setEnviando(true);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setEnviando(true);
 
-//     if (!validacao()) {
-//       setEnviando(false);
-//       return;
-//     }
+        if (email.trim() === '') {
+            setErro('Campo email é obrigatório');
+            setEnviando(false);
+            return;
+        }
 
-//     // Simula o envio do e-mail para recuperação de senha
-//     setTimeout(() => {
-//       toast.success('Link para recuperação enviado para seu e-mail!', {
-//         position: "top-right",
-//         autoClose: 5000,
-//         hideProgressBar: false,
-//         closeOnClick: true,
-//         pauseOnHover: true,
-//         draggable: true,
-//         progress: undefined,
-//       });
-//       setEnviando(false);
-//       navigate('/login');
-//     }, 2000);
-//   };
+        if (!validarEmail(email)) {
+            setErro('Email incorreto!');
+            setEnviando(false);
+            return;
+        }
 
-//   return (
-//     <div className="forgot-password-container">
-//       <div className="forgot-password-bg"></div>
-//       <div className="forgot-password-modal">
-//         <h1>Recuperação de Senha</h1>
-//         <form onSubmit={handleSubmit} className="forgot-password-form">
-//           <p>Digite seu e-mail para receber um link de recuperação de senha</p>
-//           <div className="input-form">
-//             <label htmlFor="email">E-mail</label>
-//             <input
-//               type="email"
-//               id="email"
-//               placeholder="Digite seu e-mail"
-//               value={email}
-//               onChange={(e) => setEmail(e.target.value)}
-//             />
-//           </div>
-//           {erro && <span className="error">{erro}</span>}
-//           <button type="submit" className="btn-enter" disabled={enviando}>
-//             {enviando ? 'Enviando...' : 'Confirmar'}
-//           </button>
-//           <button type="button" className="btn-cancel" onClick={() => navigate('/login')}>Cancelar</button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }
+        setErro('');
+        try {
+            await new Promise((resolve) => setTimeout(resolve, 1500));
+            toast.success('Se o email existir, você receberá instruções em instantes.');
+            navigate('/');
+        } catch (error) {
+            toast.error('Erro ao tentar redefinir senha.');
+        } finally {
+            setEnviando(false);
+        }
+    };
 
-// export default ForgotPassword;
+    return (
+        <main className="forgot-background">
+            <div className="forgot-modal">
+                <img src={SLA} alt="Apollo" className="forgot-logo" />
+
+                <h1>Esqueci Minha Senha</h1>
+                <p>informe seu email para receber a definição de senha</p>
+
+                <form onSubmit={handleSubmit}>
+                    <div className="input-form">
+                        <label htmlFor="email">Email</label>
+                        <span className="icon"><At size={22} color='black' /></span>
+                        <input
+                            type="text"
+                            id="email"
+                            placeholder="Digite seu email"
+                            className={`${erro ? 'error-input' : ''}`}
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    {erro && <span className="error">{erro}</span>}
+
+                    <div className="actions-forgot">
+                        <Link to="/" className="btn-back">Voltar</Link>
+                        <button type="submit" className="btn-confirm" disabled={enviando}>
+                            Confirmar
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </main>
+    );
+}
+
+export default ForgotPassword;
